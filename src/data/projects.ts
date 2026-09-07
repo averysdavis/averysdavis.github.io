83971c93a029d6d3017282e223631860ef7d683c
Avery Davis <averydavis9@proton.me>
2026-09-07 16:50:49 -0700

    flask

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -19,7 +19,7 @@ const data: Project[] = [
     imageAlt: 'Website with a purple header and navigation menu; large white text on a wooden background reads, “The Cluck is Ticking.”.',
     date: '2026-10-20',
     desc: 'Documentation website for the KhanLab-BayArea 2025 iGEM project.',
-    tech: ['HTML', 'CSS', 'Bootstrap', 'Jinja', 'Mathjax'],
+    tech: ['HTML', 'CSS', 'Bootstrap', 'Jinja', 'Flask'],
     featured: true,
   },
   {
@@ -63,7 +63,7 @@ const data: Project[] = [
     imageAlt: 'A website with the text “Rock The Plastics!” appears over a textured gray background, with a cartoon rock character playing a red guitar on the right side.',
     date: '2025-10-20',
     desc: 'Documentation website for the KhanLab-BayArea 2026 iGEM project.',
-    tech: ['HTML', 'CSS', 'Bootstrap', 'Jinja'],
+    tech: ['HTML', 'CSS', 'Bootstrap', 'Jinja', 'Flask'],
     featured: true,
   },
   {
