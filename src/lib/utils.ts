d9cf89080110def89a6545e5f837d3cf5b8ac9ca
Avery Davis <averydavis9@proton.me>
2026-09-07 20:31:55 -0700

    updating url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@
 import profile from '@/data/profile.json';
 
 // Site configuration
-export const SITE_URL = 'https://averysdavis.github.io';
+export const SITE_URL = 'https://averysdavis.com';
 export const AUTHOR_NAME = profile.name;
 /**
  * The portrait. Used for JSON-LD `image`, where the value should be a picture
